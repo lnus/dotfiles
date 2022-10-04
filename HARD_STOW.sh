3e212a18c93aeb9e27e39e6ed0f94a2f596615c0
Linus Wæhler <linus.wahler@gmail.com>
2022-10-04 16:59:28 +0200

    Added bad script, don't use plz

---
+++ HARD_STOW.sh
@@ -0,0 +1,13 @@
+# ALERT: Don't use this, I just need it for a very specific thing.
+# ALERT: Don't use this, I just need it for a very specific thing.
+# ALERT: Don't use this, I just need it for a very specific thing.
+
+rm -rf "$HOME/.config/nvim"
+rm -f "$HOME/.vimrc"
+rm -f "$HOME/.zshrc"
+rm -f "$HOME/.p10k.zsh"
+
+ln -s "./nvim/.config/nvim/" "$HOME/.config/nvim/" 
+ln -s "./vim/.vimrc" "$HOME/.vimrc" 
+ln -s "./zsh/.zshrc" "$HOME/.zshrc"
+ln -s "./zsh/.p10k.zsh" "$HOME/.p10k.zsh"
