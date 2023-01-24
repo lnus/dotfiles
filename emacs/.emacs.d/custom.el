68c3aabc24f5067ba81a46268f29eff983237d56
Linus Wähler <linus.wahler@gmail.com>
2023-01-24 17:17:03 +0100

    Changed how plugins are handled

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -1,27 +1,30 @@
-(setq inhibit-startup-message t
-      initial-scratch-message "Hello world"
-      cursor-type 'bar)
-
+;; TODO: Move this to a init-packages.el
 ;; Define and initialise package repos
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
+;; List packages I want
+(setq package-list
+      '(evil magit minimap))
+
+;; Init packages
 (package-initialize)
-(package-refresh-contents)
 
-;; use-package to simplify the config file
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-(require 'use-package)
-(setq use-package-always-ensure 't)
+;; Fetch available packages 
+(unless package-archive-contents
+  (package-refresh-contents))
 
-;; Download Evil
-(unless (package-installed-p 'evil)
-  (package-install 'evil))
+;; Install missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
 
 ;; Enable Evil
 (require 'evil)
 (evil-mode 1)
+
+;; Enable mini-map
+(minimap-mode 1)
 
 ;; Display line numbers
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
@@ -32,16 +35,12 @@
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (defalias 'yes-or-no-p 'y-or-n-p)
-
-;; Theme
-(use-package exotica-theme
-  :config (load-theme 'exotica t))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(exotica-theme)))
+ '(package-selected-packages '(minimap evil exotica-theme magit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
