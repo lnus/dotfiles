8011da05c1aa2d7012c312ed66833a8ed543924a
Linus Wähler <linus.wahler@gmail.com>
2023-01-24 17:01:42 +0100

    Decided to start torturing myself

---
+++ emacs/.emacs.d/custom.el
@@ -0,0 +1,50 @@
+(setq inhibit-startup-message t
+      initial-scratch-message "Hello world"
+      cursor-type 'bar)
+
+;; Define and initialise package repos
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+(package-refresh-contents)
+
+;; use-package to simplify the config file
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure 't)
+
+;; Download Evil
+(unless (package-installed-p 'evil)
+  (package-install 'evil))
+
+;; Enable Evil
+(require 'evil)
+(evil-mode 1)
+
+;; Display line numbers
+(add-hook 'prog-mode-hook 'display-line-numbers-mode)
+
+;; Keyboard-centric user interface
+(setq inhibit-startup-message t)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; Theme
+(use-package exotica-theme
+  :config (load-theme 'exotica t))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(exotica-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
