4bc512a959e079503c25bec6b219ef2ee3b2a4bc
Linus Wæhler <linus.wahler@gmail.com>
2024-03-21 23:16:51 +0100

    LSP setup

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -2,4 +2,6 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set number")
+vim.wo.relativenumber = true
 vim.g.mapleader = " "
