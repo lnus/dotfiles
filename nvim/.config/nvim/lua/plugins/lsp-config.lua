c08a92ccd6bfc803a5ee00c675892f7187df131f
Linus Wæhler <linus.wahler@gmail.com>
2024-03-21 23:33:34 +0100

    Formatters and stuff

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -24,9 +24,6 @@ return {
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
-      vim.keymap.set('n', '<leader>f', function()
-        vim.lsp.buf.format { async = true }
-      end, {})
     end
   }
 }
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -5,8 +5,9 @@ return {
     local config = require("nvim-treesitter.configs")
     config.setup({
       ensure_installed = { "lua", "javascript" },
+      auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
     })
-  end
+  end,
 }
