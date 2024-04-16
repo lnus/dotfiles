9fa4dbe3028dbe1808be10438d140dfeb517a25b
Linus Wæhler <linus.wahler@gmail.com>
2024-04-16 22:54:04 +0200

    added lazyvim
    holy shit the defaults are so SANE

---
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -0,0 +1,15 @@
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- add moonfly
+  { "bluz71/vim-moonfly-colors" },
+
+  -- set default colors
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "moonfly",
+    },
+  },
+}
