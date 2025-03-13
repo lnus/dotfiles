e753a6a64b989eaacaebc937d8a9ce22ea3835a7
Linus Wæhler <17277861+lnus@users.noreply.github.com>
2025-03-13 01:18:02 +0100

    added jira.fish completions
    thanks claude

---
+++ dot_config/fish/completions/jira.fish
@@ -0,0 +1,11 @@
+complete -c jira -f  # -f means no file completion
+
+complete -c jira -l details -d "Show detailed locations of all items"
+
+# TODO: Implement all of these
+#complete -c jira -l todo -d "Show only TODO items"
+#complete -c jira -l fix -d "Show only FIX items"
+#complete -c jira -l fixme -d "Show only FIXME items"
+#complete -c jira -l bug -d "Show only BUG items"
+#complete -c jira -l sort -d "Sort items by file or count" -a "file count"
+#complete -c jira -l format -d "Output format" -a "text json csv"
